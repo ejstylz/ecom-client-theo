546b12d547a767034d5129186f23886e4c73b1ad
Theophilus Okwugwuni <theophilusdev@gmail.com>
2021-10-22 15:25:01 +0100

    connect to api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import "./App.css";
+import AppBar from "./widgets/Appbar";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
+  const [artWork, setArtwork] = useState({});
+  const [open, setOpen] = useState(false);
+  let [id, setId] = useState(100);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  async function fetchData() {
+    try {
+      const response = await axios.get(`http://localhost:4000/api?id=${id}`);
+      if (response.status === 200) {
+        setArtwork(response.data);
+        setId(id++);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      fetchData();
+    }, 10000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar />
+      <div className="container">
+        {artWork && artWork.primaryImage ? (
+          <div className="main">
+            <img src={artWork.primaryImage} alt="art work" onClick={handleOpen} />
+            <h4>Name: {artWork.objectName}</h4>
+
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <div>
+                  <p>Credit: {artWork.creditLine}</p>
+                  <p>Country: {artWork.country}</p>
+                  <p>Dimensions: {artWork.dimensions}</p>
+                  <p>Culture: {artWork.culture}</p>
+                  <p>Department: {artWork.department}</p>
+                  <p>Repository: {artWork.repository}</p>
+                </div>
+              </Box>
+            </Modal>
+          </div>
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </div>
+    </>
   );
 }
 
